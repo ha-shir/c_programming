d0d98c9da8c9e9b87e944e176518848e36d45f49
ha-shir <hashir00710@gmail.com>
2025-05-19 10:03:42 +0530

    even or odd

--- main.c
+++ main.c
@@ -1,22 +1,25 @@
 /******************************************************************************
 
 Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
 #include <stdio.h>
 
 int main()
 {
-    int i;
-    
-    for(i=2;i<=50;i++){
-        if(i%2==0);
-        printf("%d,",i);
-    }
-    
+     int num1=;
+     if(num1>0){
+         printf("number is positive");
+         
+     }
+      else{
+          printf("number is negative");
+      }
+ 
+     
 
     return 0;
 }
