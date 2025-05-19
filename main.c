4f728c1ba7d8be6596754b11815586749f994e9a
ha-shir <hashir00710@gmail.com>
2025-05-19 09:59:22 +0530

    even-of-50-using-for-loop

--- main.c
+++ main.c
@@ -1,22 +1,22 @@
 /******************************************************************************
 
 Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
 #include <stdio.h>
 
 int main()
 {
-    int i=0;
-    while(i<=50){
-    i++;
-    if(i%2==0)
-    {
-    printf("%d\n",i);
-}
-}
+    int i;
+    
+    for(i=2;i<=50;i++){
+        if(i%2==0);
+        printf("%d,",i);
+    }
+    
+
     return 0;
 }
