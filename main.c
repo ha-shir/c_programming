7c90ae2cea20f2cb01ea4f00b2967e556430d438
ha-shir <hashir00710@gmail.com>
2025-05-24 18:05:28 +0530

    hollow triangle

--- main.c
+++ main.c
@@ -1,20 +1,28 @@
 /******************************************************************************
 
 Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
 #include <stdio.h>
 
 int main()
 {
- int i;
+ int i,j,size=5;
  
- for(i=0;i<=5;i++){
- printf("%d\n", i );
-}
-    
+ for(i=1;i<=5;i++){
+     for(j=1;j<=5;j++){
+         if( i==size || j==1 || j==i){
+             printf("*");
+         }
+         else{
+             printf(" ");
+         }
+     }
+     printf("\n");
+ }
+
     return 0;
 }
